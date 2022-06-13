c8b690b11190e36a50d04ae03790264e8b8152eb
ML6503 <ml6503@gmail.com>
2022-06-13 03:51:29 +0300

    feat: unit tests added

--- shop-service/src/functions/getProductsById/handler.ts
+++ shop-service/src/functions/getProductsById/handler.ts
@@ -5,24 +5,29 @@ import ProductService from 'src/service/productService';
 
 import schema from './schema';
 
-const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  let product = null;
-  console.log('event from getProductById: ', event);
+export const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  try {
+    let product = null;
+    console.log('event from getProductById: ', event);
 
-  if (event.pathParameters && event.pathParameters.productId) {
-    const { productId } = event.pathParameters;
-    const productService = new ProductService;
-    product = await productService.getProductById(productId);    
+    if (event.pathParameters && event.pathParameters.productId) {
+      const { productId } = event.pathParameters;
+      const productService = new ProductService;
+      product = await productService.getProductById(productId);    
+    }
+
+    return product
+      ? formatJSONResponse({
+          product: product,    
+        })
+      : {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'Product not found' })
+      };
+
+  } catch (e) {
+    console.error(e);
   }
-
-  return product
-    ? formatJSONResponse({
-        product: product,    
-      })
-    : {
-      statusCode: 404,
-      body: JSON.stringify({ error: 'Product not found' })
-    };
 };
 
 export const main = middyfy(getProductsById);
--- shop-service/src/functions/getProductsList/handler.ts
+++ shop-service/src/functions/getProductsList/handler.ts
@@ -2,14 +2,18 @@ import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import ProductService from 'src/service/productService';
 
-const getProductsList = async () => {
-  const productService = new ProductService;
-  const allProducts = await productService.getAllProducts();
+export const getProductsList = async () => {
+ try {
+    const productService = new ProductService;
+    const allProducts = await productService.getAllProducts();
 
-  return formatJSONResponse({
-    products: allProducts,
-    
-  });
+    return formatJSONResponse({
+      products: allProducts,
+      
+    });
+ } catch (e) {
+    console.error(e);
+ }
 }
 
 export const main = middyfy(getProductsList);
