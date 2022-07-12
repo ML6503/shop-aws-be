bb6ed0d111499ace8c4a86975a34f7fd9a6864c6
ML6503 <ml6503@gmail.com>
2022-07-12 23:16:40 +0300

    feat: add invoke of lambda add product from lambda catalog batch process

--- src/import-service/handlers/catalogBatchProcess.js
+++ src/import-service/handlers/catalogBatchProcess.js
@@ -1,24 +1,47 @@
 const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
+const AWS = require('aws-sdk');
 const { ACCEPTED } = require('http-status');
 const { unhandledErrorCatch } = require('../common/error');
-const { ProductService } = require('../dbService/productService');
+// const { ProductService } = require('../dbService/productService');
+const snsClient = new SNSClient({ region: process.env.REGION });
+AWS.config.region = process.env.REGION;
+const lambda = new AWS.Lambda();
 
-
-module.exports.catalogBatchProcess = async (event) => {
+module.exports.catalogBatchProcess = async (event, context) => {
     console.log('We are in BatchProcess!!');
     try {
-        const snsClient = new SNSClient({ region: process.env.REGION });
-        
-        const productService = new ProductService();
+        // const productService = new ProductService();
+        let addedProduct;
 
         await event.Records.map(async ({ body }) => {
             // get new product from event and add to Data Base
-            let newProduct = await JSON.parse(body);
-            console.log('NEW PRODUCT DATA FROM BODY', newProduct);
+            // let newProduct = await JSON.parse(body);
+            // console.log('NEW PRODUCT DATA FROM BODY', newProduct);
+            console.log('NEW PRODUCT DATA FROM BODY', body);
 
-            let addedProduct = await productService.addProduct(newProduct);
-   
-            console.log('addedProduct ', addedProduct );
+            let invokeParams = {
+                FunctionName: 'addProduct',
+                InvocationType: 'RequestResponse',
+                LogType: 'Tail',
+                PayLoad: body,
+            };
+
+            await lambda
+                .invoke(invokeParams, (err, data) => {
+                    if (err) {
+                        context.fail(err);
+                    } else {
+                        addedProduct = data.Payload;
+                        context.succeed(
+                            `New Product ${data.Payload.title} Has been added`
+                        );
+                    }
+                })
+                .promise();
+
+            // let addedProduct = await productService.addProduct(newProduct);
+
+            console.log('addedProduct ', addedProduct);
             // publish at sns topic
             const publishCommandParams = {
                 Subject: 'New product has been added to DB',
@@ -27,16 +50,14 @@ module.exports.catalogBatchProcess = async (event) => {
                 MessageAttributes: {
                     title: {
                         DataType: 'String',
-                        StringValue: addedProduct.title
-                    }
-                }
+                        StringValue: addedProduct.title,
+                    },
+                },
             };
             const publishCommand = new PublishCommand(publishCommandParams);
 
             await snsClient.send(publishCommand);
-
-        });    
-      
+        });
     } catch (err) {
         console.error(err);
     }
