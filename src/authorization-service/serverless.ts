de6b85f48faa6a38615d092d5bdfddc95ed104cd
ML6503 <ml6503@gmail.com>
2022-07-19 17:43:02 +0300

    feat: add region and cors to serverless

--- src/authorization-service/serverless.ts
+++ src/authorization-service/serverless.ts
@@ -12,6 +12,10 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'eu-west-1',
+    httpApi: {
+      cors: true
+    }, 
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
