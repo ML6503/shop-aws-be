fe58d7063dd59b9a8ebd8bc5b35ae5498e7a052c
ML6503 <ml6503@gmail.com>
2022-07-22 00:05:41 +0300

    feat: add gateaway reponse header for auth service

--- src/authorization-service/serverless.ts
+++ src/authorization-service/serverless.ts
@@ -2,46 +2,73 @@ import type { AWS } from '@serverless/typescript';
 
 import basicAuthorizer from '@functions/basicAuthorizer.ts';
 
-
 const serverlessConfiguration: AWS = {
-  service: 'authorization-service',
-  frameworkVersion: '3',
-  plugins: [
-    'serverless-esbuild',
-    'serverless-dotenv-plugin'
-  ],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    region: 'eu-west-1',
-    logRetentionInDays: 14,
-    httpApi: {
-      cors: true,    
-    }, 
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
+    service: 'authorization-service',
+    frameworkVersion: '3',
+    plugins: ['serverless-esbuild', 'serverless-dotenv-plugin'],
+    provider: {
+        name: 'aws',
+        runtime: 'nodejs14.x',
+        region: 'eu-west-1',
+        logRetentionInDays: 14,
+        httpApi: {
+            cors: true,
+        },
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+            NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+        },
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    // import the function via paths
+    functions: { basicAuthorizer },
+    package: { individually: true },
+    custom: {
+        esbuild: {
+            bundle: true,
+            minify: false,
+            sourcemap: true,
+            exclude: ['aws-sdk'],
+            target: 'node14',
+            define: { 'require.resolve': undefined },
+            platform: 'node',
+            concurrency: 10,
+        },
     },
-  },
-  // import the function via paths
-  functions: { basicAuthorizer },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
+    resources: {
+        Resources: {
+            ApiGatewayRestApi: {
+                Type: 'AWS::ApiGateway::RestApi',
+                Properties: {
+                    Name: 'authorization-service-cyprushandmade-dev',
+                },
+            },
+            GatewayResponseDefault4XX: {
+                Type: 'AWS::ApiGateway::GatewayResponse',
+                Properties: {
+                    RestApiId: {
+                        Ref: 'ApiGatewayRestApi',
+                    },
+                    ResponseParameters: {
+                        'gatewayresponse.header.Access-Control-Allow-Origin':
+                            "'*'",
+                        'gatewayresponse.header.Access-Control-Allow-Headers':
+                            "'*'",
+                        'gatewayresponse.header.Access-Control-Allow-Methods':
+                            "'GET,OPTIONS'",
+                    },
+                    ResponseType: 'DEFAULT_4XX',
+                    ResponseTemplates: {
+                        'application/jso':
+                            '{"error":{"code":"custom-4XX-generic","message":$context.error.messageString},"requestId":"$context.requestId"}',
+                    },
+                },
+            },
+        },
     },
-  },
 };
 
 module.exports = serverlessConfiguration;
