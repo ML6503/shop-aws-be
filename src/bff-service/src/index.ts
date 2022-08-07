0322ba6526da243c878ad077dac6f361de1cf9c3
ML6503 <ml6503@gmail.com>
2022-08-07 22:25:07 +0300

    feat: add cache

--- src/bff-service/src/index.ts
+++ src/bff-service/src/index.ts
@@ -2,13 +2,18 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import { BAD_GATEWAY, INTERNAL_SERVER_ERROR, OK } from 'http-status';
+import NodeCache from 'node-cache';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+const shopCache = new NodeCache({ stdTTL: 120 });
+
 app.use(express.json());
+
+app.get('/stats', (_req, res) => res.send(shopCache.getStats()));
 
 app.all('/*', (req: Request, res: Response) => {
     const method = req.method;
@@ -26,27 +31,33 @@ app.all('/*', (req: Request, res: Response) => {
     console.log('recipient URL: ', recipientURL);
 
     if (recipientURL) {
-        axios({
-            method: method,
-            url: `${recipientURL}${req.originalUrl}`,
-            ...body,
-        })
-            .then(({ data }) => {
-                console.log('Resp to BFF:', data);
-
-                res.status(OK).json(data);
+        if (shopCache.has('products')) {
+            console.log('Getting products from CACHE');
+            res.status(OK).json(shopCache.get('products'));
+        } else {
+            axios({
+                method: method,
+                url: `${recipientURL}${req.originalUrl}`,
+                ...body,
             })
-            .catch((error) => {
-                console.error('BFF Error:', JSON.stringify(error));
-                if (error) {
-                    const { status, data } = error.response;
-                    res.status(status).json(data);
-                } else {
-                    res.status(INTERNAL_SERVER_ERROR).json({
-                        error: error.message,
-                    });
-                }
-            });
+                .then(({ data }) => {
+                    console.log('Resp to BFF:', data);
+                    shopCache.set('products', data);
+                    console.log('Getting products from API');
+                    res.status(OK).json(data);
+                })
+                .catch((error) => {
+                    console.error('BFF Error:', JSON.stringify(error));
+                    if (error) {
+                        const { status, data } = error.response;
+                        res.status(status).json(data);
+                    } else {
+                        res.status(INTERNAL_SERVER_ERROR).json({
+                            error: error.message,
+                        });
+                    }
+                });
+        }
     } else {
         res.status(BAD_GATEWAY).json({ error: 'Cannot process request' });
     }
@@ -55,3 +66,5 @@ app.all('/*', (req: Request, res: Response) => {
 app.listen(+port, () =>
     console.log(`Server is running at http://localhost:${port}`)
 );
+
+export default app;
